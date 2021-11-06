2c9910698b4e613f4fa608484119dbc2295534ab
bcalisaya4 <u17200830@utp.edu.pe>
2021-11-06 10:43:33 -0500

    vista de registro login

--- app/src/main/java/com/example/violencia_0_app/MainActivity.java
+++ app/src/main/java/com/example/violencia_0_app/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.violencia_0_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn_registro,btn_panico,btn_login,btn_info,btn_otros;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_registro =findViewById(R.id.btn_register);
+        btn_login=findViewById(R.id.btn_login);
+        btn_info=findViewById(R.id.btn_inf);
+        btn_otros=findViewById(R.id.btn_other);
+        //btn_panico=findViewById(R.id.btn_panico);
+        // action button
+        btn_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegistroUsuarios.class));
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),login.class));
+            }
+        });
+
     }
 }
